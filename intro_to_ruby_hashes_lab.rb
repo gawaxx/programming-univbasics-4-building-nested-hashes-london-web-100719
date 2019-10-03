f121e899556b54d5407531010bba5d7e6efb87e3
jules blanc <jules.blanc@unccub.com>
2019-10-03 15:08:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,10 +17,10 @@ def monopoly_with_third_tier
     :four_pieces_owned => 200
       },
   :names => {
-      :reading_railroad => {"mortgage_value"=>"$100"},
-      :pennsylvania_railroad => {"mortgage_value"=>"$200" },
-      :b_and_o_railroad => {"mortgage_value"=>"$400"},
-      :shortline => {"mortgage_value"=>"$800"}
+      :reading_railroad => {"mortgage_value" => {} },
+      :pennsylvania_railroad => {"mortgage_value" => {} },
+      :b_and_o_railroad => {"mortgage_value" => {}},
+      :shortline => {"mortgage_value" => {}}
     
     }
   }
