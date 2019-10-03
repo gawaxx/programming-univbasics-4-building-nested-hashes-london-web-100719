90e199c4d090ba97343a4bb0cb31f3ac34649dbe
jules blanc <jules.blanc@unccub.com>
2019-10-03 14:57:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,11 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  monopoly = {:railroads => {}}
+  monopoly = {:railroads => {:pieces => {}
+    
+  }
+    
+  }
 end
 
 def monopoly_with_third_tier
