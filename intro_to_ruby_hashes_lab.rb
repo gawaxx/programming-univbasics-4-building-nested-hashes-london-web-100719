92dbac8c76496b5f1ca1bee82587d2304cde1c71
jules blanc <jules.blanc@unccub.com>
2019-10-03 15:01:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,10 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  monopoly = {:railroads => {:pieces => 4, :rent_in_dollars => {:key1, :key2, :key3, :key4 }}}
+  monopoly = {:railroads => 
+  {
+  :pieces => 4, 
+  :rent_in_dollars => {:key1, :key2, :key3, :key4 }}}
 end
 
 def monopoly_with_fourth_tier
